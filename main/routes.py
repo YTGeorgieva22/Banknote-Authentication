89a5cb315dbb2dc2dfbcb2762cb9db2976852025
Yoana Georgieva <YTGeorgieva22@codingburgas.bg>
2026-04-10 14:37:07 +0300

    Develop the home page for non-logged users

--- main/routes.py
+++ main/routes.py
@@ -4,7 +4,7 @@ from . import main_bp
 current_user = None
 @main_bp.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('base.html', current_user=current_user)
+    return render_template('index.html', current_user=current_user)
 @main_bp.route('/home')
 def home():
     return render_template('home.html', current_user=current_user)
